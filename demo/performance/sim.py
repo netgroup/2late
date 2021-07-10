699bd7f5275ed8bbd9f127738700ae4bf8372526
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2021-07-10 15:52:50 +0200

    improved sim

--- demo/performance/sim.py
+++ demo/performance/sim.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from toolate import encrypt as toolate_encrypt, decrypt as toolate_decrypt
+from aes import encrypt as aes_encrypt, decrypt as aes_decrypt
+
 from time import perf_counter
 
 
@@ -8,22 +10,29 @@ plaintext = b"ciaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaoci
 ciphertext = None
 iterations = 1000
 
-encrypt, decrypt = toolate_encrypt, toolate_decrypt
+encryptors = {
+    "toolate": {"encrypt": toolate_encrypt, "decrypt": toolate_decrypt},
+    "aes": {"encrypt": aes_encrypt, "decrypt": aes_decrypt},
+}
 
-# Start the stopwatch / counter
-t1_start = perf_counter()
-for i in range(iterations):
-    ciphertext = encrypt(plaintext)
-    #print(f"ciphertext: {ciphertext}")
-# Stop the stopwatch / counter
-t1_stop = perf_counter()
-print(f"Encryption time for {iterations} iterations:", t1_stop-t1_start)
+for enc, functions in encryptors.items():
+    print(f"Testing {enc}")
+    encrypt, decrypt = functions["encrypt"], functions["decrypt"]
 
-# Start the stopwatch / counter
-t1_start = perf_counter()
-for i in range(iterations):
-    decoded = decrypt(ciphertext)
-    #print(f"plaintext: {decoded}")
-# Stop the stopwatch / counter
-t1_stop = perf_counter()
-print(f"Decryption time for {iterations} iterations:", t1_stop-t1_start)
+    # Start the stopwatch / counter
+    t1_start = perf_counter()
+    for i in range(iterations):
+        ciphertext = encrypt(plaintext)
+        #print(f"ciphertext: {ciphertext}")
+    # Stop the stopwatch / counter
+    t1_stop = perf_counter()
+    print(f"Encryption time for {iterations} iterations:", t1_stop-t1_start)
+
+    # Start the stopwatch / counter
+    t1_start = perf_counter()
+    for i in range(iterations):
+        decoded = decrypt(ciphertext)
+        #print(f"plaintext: {decoded}")
+    # Stop the stopwatch / counter
+    t1_stop = perf_counter()
+    print(f"Decryption time for {iterations} iterations:", t1_stop-t1_start)
